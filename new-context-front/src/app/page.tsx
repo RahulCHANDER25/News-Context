e9f388b10412c2f8f33fdf37dddd30574bea586a
rahul.chander <rahul.chander@epitech.eu>
2025-01-31 15:48:29 +0100

    refactor: File architecture is now setup

--- new-context-front/src/app/page.tsx
+++ new-context-front/src/app/page.tsx
@@ -1,5 +1,12 @@
 import Image from "next/image";
 
+/**
+ * pages:
+ * /analyze/article
+ * /analyze/journalist
+ * /analyze/newspaper
+ */
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -16,7 +23,7 @@ export default function Home() {
           <li className="mb-2">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
+              src/app/page.tsx
             </code>
             .
           </li>
