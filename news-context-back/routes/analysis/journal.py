1378f1ebd5250f9f7a11270c4cb293a963d31b42
rahul.chander <rahul.chander@epitech.eu>
2025-02-02 22:34:20 +0100

    feat: Minimal API with ollama and backend

--- news-context-back/routes/analysis/journal.py
+++ news-context-back/routes/analysis/journal.py
@@ -1,10 +1,27 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+import requests
 
 router = APIRouter(
     prefix="/analysis"
 )
 
+headers = {
+    "Content-Type": "application/json"
+}
+
 @router.post("/journal")
 async def journal_analysis():
-
+    try:
+        res = requests.post(
+            url="http://localhost:11434/api/generate",
+            json={"model": "mistral", "prompt": "Say Hello in a few lines !", "stream": False},
+            headers=headers
+        )
+        res.raise_for_status()
+        return {"generated_text": res.json()["response"]}
+    except requests.RequestException as e:
+        print(e)
+        raise HTTPException(status_code=500, detail=e.__str__())
     return {}
+
+## List current available ollama models
